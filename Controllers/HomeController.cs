488883b0c1e921e1eccae6f79ba127a6cc51e35e
nannkau <48847934+nannkau@users.noreply.github.com>
2020-06-07 20:54:16 +0700

    xong tim kiem

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MyBakeryShop.Models;
 using MyBakeryShop.ViewModels;
-
+//ok
 namespace MyBakeryShop.Controllers
 {
     public class HomeController : Controller
