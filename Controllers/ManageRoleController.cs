c2aaef9bb831003cfea2510825235e5564e29be5
nannkau <48847934+nannkau@users.noreply.github.com>
2020-06-14 11:41:13 +0700

    commit

---
+++ Controllers/ManageRoleController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MyBakeryShop.Models.Data;
+
+namespace MyBakeryShop.Controllers
+{
+    public class ManageRoleController : Controller
+    {
+        private readonly BakeryDbContext _bakeryDbContext;
+        public ManageRoleController(BakeryDbContext bakeryDbContext)
+        {
+            _bakeryDbContext = bakeryDbContext;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var model = _bakeryDbContext.Roles.AsAsyncEnumerable();
+            return View(model);
+        }
+        public ViewResult Create()
+
+        {
+
+            return View();
+
+        }
+
+        [HttpPost]
+
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Create([Bind("Id,Name")] IdentityRole role)
+
+        {
+
+            try
+
+            {
+
+                if (ModelState.IsValid)
+
+                {
+
+                    _bakeryDbContext.Roles.Add(role);
+
+                    _bakeryDbContext.SaveChanges();
+
+                }
+
+                return RedirectToAction("Index");
+
+            }
+
+            catch (Exception ex)
+
+            {
+
+                ModelState.AddModelError("", ex.Message);
+
+            }
+
+            return View(role);
+
+        }
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var banner =  _bakeryDbContext.Roles
+                .Find(id);
+            if (banner == null)
+            {
+                return NotFound();
+            }
+
+            return View(banner);
+        }
+
+        // POST: AdminBanners/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var banner = await _bakeryDbContext.Roles.FindAsync(id);
+            _bakeryDbContext.Roles.Remove(banner);
+            await _bakeryDbContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Controllers/ManageUserController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AngleSharp.Dom;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MyBakeryShop.Models.Data;
+
+namespace MyBakeryShop.Controllers
+{
+    public class ManageUserController : Controller
+    {
+        private readonly BakeryDbContext _bakeryDbContext;
+        public ManageUserController(BakeryDbContext bakeryDbContext)
+        {
+            _bakeryDbContext = bakeryDbContext;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<IdentityUser> model = _bakeryDbContext.Users.AsEnumerable();
+
+            return View(model);
+        }
+        public ActionResult Edit(string Id)
+
+        {
+
+            IdentityUser model = _bakeryDbContext.Users.Find(Id);
+
+            return View(model);
+
+        }
+        [HttpPost]
+
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Edit(IdentityUser model)
+
+        {
+
+            try
+
+            {
+
+                _bakeryDbContext.Entry(model).State = EntityState.Modified;
+
+                _bakeryDbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+
+            }
+
+            catch (Exception ex)
+
+            {
+
+                ModelState.AddModelError("", ex.Message);
+
+                return View(model);
+
+            }
+
+        }
+        public ActionResult EditRole(string Id)
+
+        {
+
+            IdentityUser model = _bakeryDbContext.Users.Find(Id);
+
+            ViewBag.RoleId = new SelectList(_bakeryDbContext.Roles.ToList().Where(item => model.Id == item.Id).ToList(), "Id", "Name");
+
+            return View(model);
+
+        }
+
+
+       
+
+
+    }
+}
